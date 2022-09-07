2f5dcd2e31b5f15f73f804d2a086c0ef0d2662f2
Howard M. Lewis Ship <hlship@gmail.com>
2022-09-07 15:37:20 -0700

    Very basic demo

--- src/net/lewisship/dex/views.cljs
+++ src/net/lewisship/dex/views.cljs
@@ -1,5 +1,8 @@
 (ns net.lewisship.dex.views
   (:require
+    ["react" :as react]
+    ["react-flow-renderer" :as rfr :default ReactFlow]
+    [reagent.core :as r]
     [re-frame.core :as rf]
     [net.lewisship.dex.common :refer [<sub event-dispatch]]))
 
@@ -45,13 +48,34 @@
   []
   [:div (<sub [::label])])
 
+(defn flow
+  []
+  (let [nodes [{:id "1"
+                :type "input"
+                :data {:label "io.pedestal/jetty 1.0.0"}
+                :position {:x 250 :y 25}}
+               {:id "2"
+                :type "output"
+                :data {:label "io.pedestal/service\n1.0.0"}
+                :position {:x 250 :y 100}}]
+        edges [{:id "1-2"
+                :source "1"
+                :target "2"}]]
+    [:div.app-flow-container
+     [:> ReactFlow {:nodes (clj->js nodes)
+                    :edges (clj->js edges)}]]))
+
 (defn click-view
   []
   [:div
    [inc-count-label]
    [:br]
    [increment-button]
-   [reset-button]])
+   [reset-button]
+   [:hr]
+   [flow]
+   [:hr]
+   [:b "After Flow"]])
 
 (defn init
   []
